bd7586832c3ad3f54dfd87640f440e3c0f3b1fd5
iamtaufik <muhtopik07@gmail.com>
2024-05-14 19:50:03 +0700

    fix: server side session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { getServerAuthSession } from "@/server/auth";
 import Login from "./_components/login-page";
 import { Suspense } from "react";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await getServerAuthSession();
+
+  if (session?.user) return redirect("/dashboard");
+
   return (
     <Suspense>
       <Login />
